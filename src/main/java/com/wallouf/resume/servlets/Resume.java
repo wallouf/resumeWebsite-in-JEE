034aaf75966b2a7b7c90c03d065716eab72f8d48
Wallouf <ct13860@gmail.com>
2014-10-20 11:39:22 -0400

    resumeWebsite from PHP to JEE
    Transfert website from PHP platform to JEE

---
+++ src/main/java/com/wallouf/resume/servlets/Resume.java
@@ -0,0 +1,19 @@
+package com.wallouf.resume.servlets;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Servlet implementation class Connection
+ */
+@Controller
+@RequestMapping( "/" )
+public class Resume {
+
+    @RequestMapping( method = RequestMethod.GET )
+    public String afficherBonjour( final ModelMap pModel ) {
+        return "portfolio_display_mobile";
+    }
+}
